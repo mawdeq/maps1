da4643075d797c59c3e1fbb7b7fd81ecebf3d0a8
mawdeq <151653392+mawdeq@users.noreply.github.com>
2024-02-06 18:10:40 +0300

    Add files via upload

---
+++ maps1/задача 1.py
@@ -0,0 +1,49 @@
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        uic.loadUi('main_window.ui', self)
+        self.press_delta = 0.00001
+        self.map_zoom = 5
+        self.map_ll = [37.977751, 55.757718]
+        self.map_l = 'map'
+        self.map_key = ''
+
+        self.refresh_map()
+
+
+    def refresh_map(self):
+        map_params = {
+            "ll": ','.join(map(str, self.map_ll)),
+            "l": self.map_l,
+            'z': self.map_zoom
+        }
+        session = requests.Session()
+        retry = Retry(total=10, connect=5, backoff_factor=0.5)
+        adapter = HTTPAdapter(max_retries=retry)
+        session.mount('http://', adapter)
+        session.mount('https://', adapter)
+        response = session.get('https://static-maps.yandex.ru/1.x/',
+                                params=map_params)
+        print(response.content)
+        with open('tmp.png', mode='wb') as tmp:
+            tmp.write(response.content)
+
+        pixmap = QPixmap()
+        pixmap.load('tmp.png')
+        self.g_map.setPixmap(pixmap)
+
+
+app = QApplication(sys.argv)
+main_window = MainWindow()
+main_window.show()
+sys.exit(app.exec())
